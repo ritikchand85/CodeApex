ef5165ea6c0caa52b8c480840515153e06cd6e46
Ritik <ritik85@gmail.com>
2025-08-29 08:43:19 +0530

    fourth commit

--- Frontend/src/utils/axiosClient.js
+++ Frontend/src/utils/axiosClient.js
@@ -1,12 +1,14 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
     }
 });
+
+// console.log(import.meta.env.BASE_URL);
 
 
 export default axiosClient;
